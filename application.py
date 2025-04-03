2ba554dc17d38409450656508d3df47dd540764c
Susruth Puppala <abhirama99@live.com>
2025-04-03 13:26:56 +0100

    WHY TF IS THE DB TABLES NOT BEING CREATED

--- application.py
+++ application.py
@@ -1,21 +1,24 @@
 from sqlalchemy import text
 from flask import Flask, render_template, request
-from db import db
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-databaseName = 'InventoryDatabase'
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{databaseName}.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy()
 
-db.init_app(app)
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///InventoryDatabase.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.init_app(app)
 
-#inventory = []
+    return app
+
+app = create_app()
 
 @app.route('/')
 def index():
     test_db()
-    return render_template('index.html', inventory = inventory)
+    return render_template('index.html', inventory=inventory)
+
 
 @app.route('/inventory')
 def inventory():
@@ -32,9 +35,8 @@ def add_remove_product():
     print(product_name, quantity)
     return render_template("add_remove_product.html")
 
-def create_tables():
-    with app.app_context():
-        db.create_all()
+
+
 
 def test_db():
     try:
@@ -44,9 +46,6 @@ def test_db():
         errorText = str(ex)
         return errorText
 
-#app.app_context().push()
-
-create_tables()
 
 if __name__ == '__main__':
     app.run(debug=True)
